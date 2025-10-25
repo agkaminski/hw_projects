d3893e94679fbaf21b413f59b04c3016251aaaf6
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2025-10-25 23:13:50 +0200

    skadis_clock: Adapt 7seg decoder

--- skadis_clock/fw/ledclock.c
+++ skadis_clock/fw/ledclock.c
@@ -203,13 +203,13 @@ static byte decode7seg(byte dig)
 {
 	static const byte lut[] = {
 		0x3f, /* 0 */
-		0x06, /* 1 */
+		0x49, /* 1 */
 		0x5b, /* 2 */
 		0x4f, /* 3 */
 		0x66, /* 4 */
 		0x6d, /* 5 */
 		0x7d, /* 6 */
-		0x07, /* 7 */
+		0x27, /* 7 */
 		0x7f, /* 8 */
 		0x6f, /* 9 */
 		0x00, /* LED_VOID */
